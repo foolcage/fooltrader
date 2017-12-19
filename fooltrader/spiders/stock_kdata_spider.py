f821f44b6c6923b9f78d9d869f40979cfceecdbc
xuanqi <5533061@qq.com>
2017-12-19 21:13:52 +0800

    fix

--- fooltrader/spiders/stock_kdata_spider.py
+++ fooltrader/spiders/stock_kdata_spider.py
@@ -32,7 +32,9 @@ class StockKDataSpider(scrapy.Spider):
 
     def yield_request(self, item, trading_dates=[], fuquan=None):
         the_quarters = []
+        force_download = False
         if trading_dates:
+            force_download = True
             for the_date in trading_dates:
                 the_quarters.append(get_year_quarter(the_date))
         else:
@@ -51,7 +53,7 @@ class StockKDataSpider(scrapy.Spider):
                 data_path = get_kdata_path(item, source='sina', year=year, quarter=quarter, fuquan=fuquan)
                 data_exist = os.path.exists(data_path) or kdata_exist(item, year, quarter, fuquan, source='sina')
 
-                if not data_exist:
+                if not data_exist or force_download:
                     url = self.get_k_data_url(item['code'], year, quarter, fuquan)
                     yield Request(url=url, headers=DEFAULT_KDATA_HEADER,
                                   meta={'path': data_path, 'item': item, 'fuquan': fuquan},
