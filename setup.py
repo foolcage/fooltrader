5d9bb3a17df0192621f2a13cd3a66bb6cf7fc0d4
xuanqi <qi.xuan@56qq.com>
2018-03-23 14:21:50 +0800

    add missing requirement in setup.py

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Scrapy','Twisted','demjson','elasticsearch-dsl','kafka_python','openpyxl','xlrd','pandas','pytdx','apscheduler'],  # Optional
+    install_requires=['requests', 'Scrapy','Twisted','demjson','elasticsearch-dsl','kafka_python','openpyxl','xlrd','pandas','pytdx','apscheduler'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
